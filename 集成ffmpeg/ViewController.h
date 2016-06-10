896558ca7ac0d107706531552347608147e0efa4
txy <txy@txydeimac.local>
2016-06-10 12:41:11 +0800

    Initial Commit

---
+++ 集成ffmpeg/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  集成ffmpeg
+//
+//  Created by txy on 16/6/10.
+//  Copyright © 2016年 txy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 集成ffmpeg/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  集成ffmpeg
+//
+//  Created by txy on 16/6/10.
+//  Copyright © 2016年 txy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
